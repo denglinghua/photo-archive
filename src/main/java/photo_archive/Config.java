6fe63770514d88a883b5dc8e9d4af089caedfc74
denglinghua <denglinghua@aobi.com>
2022-01-25 17:53:46 +0800

    change to maven project

---
+++ src/main/java/photo_archive/Config.java
@@ -0,0 +1,9 @@
+package photo_archive;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Config {
+    public final static List<String> imageExtentions = Arrays.asList("jpg", "jpeg", "png");
+    public final static List<String> videoExtensions = Arrays.asList("mov", "avi", "mp4", "m4v");    
+}
---
+++ src/main/java/photo_archive/Utils.java
@@ -0,0 +1,48 @@
+package photo_archive;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.util.Set;
+
+import org.apache.commons.io.FilenameUtils;
+
+public class Utils {
+	// the name - file.txt
+	// the base name - file
+	// the extension - txt
+	static String getName(String path) {
+		return FilenameUtils.getName(path);
+	}
+
+	static String getNameWithoutExtension(String path) {
+		return FilenameUtils.getBaseName(path);
+	}
+
+	static String getFileExtension(String path) {
+		return FilenameUtils.getExtension(path);
+	}
+
+	static File[] getFileList(final File FileDir, final Set<String> acceptedExtensions) {
+		// TODO process subdirectories recursively?
+		final File[] fileList = FileDir.listFiles(new FileFilter() {
+			public boolean accept(final File file) {
+				if (file.isDirectory())
+					return false;
+				String extension = Utils.getFileExtension(file.getName()).toLowerCase();
+				if (!acceptedExtensions.contains(extension))
+					return false;
+				return true;
+			}
+		});
+
+		return fileList;
+	}
+
+	static void log(String msg) {
+		System.out.println(msg);
+	}
+
+	static void logError(String msg) {
+		System.err.println(msg);
+	}
+}
